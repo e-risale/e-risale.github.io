4830114c704891085d4549a23628108da62387c4
Kolay Risale <kolay.risale@gmail.com>
2025-12-26 18:01:12 +0300

    Add Noto Naskh Arabic to font options

--- src/reader/components/ReaderHeader.js
+++ src/reader/components/ReaderHeader.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { AnimatePresence, motion } from 'framer-motion';
 
-const FONTS = ['Lora', 'Merriweather', 'Roboto', 'Open Sans'];
+const FONTS = ['Lora', 'Merriweather', 'Roboto', 'Open Sans', 'Noto Naskh Arabic'];
 
 const ReaderHeader = ({
     showControls,
